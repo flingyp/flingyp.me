937317ece97500f77df54d5c32e3b4ace78307da
flingyp <1213563369@qq.com>
2023-04-27 16:42:40 +0800

    feat: prefect personal introduction

--- components/home/Introduction.vue
+++ components/home/Introduction.vue
@@ -2,25 +2,78 @@
 
 <template>
   <div class="space-y-8">
-    <p>✨ UnoCSS team member、Vite、Vue3、Nuxt3 & etc. Ecological contributors ✨</p>
+    <p>💞 Front-end Engineer | Open Source Enthusiast 💞</p>
 
-    <p>
-      Front-end Engineer 🧑🏻‍💻 Focus on UnoCSS, Vite, Vue & Nuxt3.
-      At the same time, I am also the Creator of Onu-UI.
-    </p>
-
-    <p>
-      A person who likes programming, likes to build all kinds of wheels and write scripts.
-      Now living in the beautiful Chengdu, China.
-    </p>
-
-    <p>
-      I have a wide range of interests,
-      watching anime, climbing mountains, listening to music,
-      learning foreign languages, playing games...
-      Recently I am learning to edit videos and I am going to be an Uploader.
-    </p>
-
-    <p>By the way, I also like to travel and take pictures, you can also simply take a look at my works 🤪.</p>
+    <div class="space-y-4">
+      <span class="font-semibold text-lg">Tech Stack</span>
+      <ul class="flex space-x-4">
+        <li class="w-6 h-6">
+          <img
+            class="w-full h-full"
+            src="https://api.iconify.design/skill-icons:vuejs-dark.svg"
+          >
+        </li>
+        <li class="w-6 h-6">
+          <img
+            class="w-full h-full"
+            src="https://api.iconify.design/skill-icons:typescript.svg"
+          >
+        </li>
+        <li class="w-6 h-6">
+          <img
+            class="w-full h-full"
+            src="https://api.iconify.design/skill-icons:vite-dark.svg"
+          >
+        </li>
+        <li class="w-6 h-6">
+          <img
+            class="w-full h-full"
+            src="https://api.iconify.design/skill-icons:nuxtjs-dark.svg"
+          >
+        </li>
+        <li class="w-6 h-6">
+          <img
+            class="w-full h-full"
+            src="https://api.iconify.design/skill-icons:tailwindcss-dark.svg"
+          >
+        </li>
+        <li class="w-6 h-6">
+          <img
+            class="w-full h-full"
+            src="https://api.iconify.design/skill-icons:nodejs-dark.svg"
+          >
+        </li>
+        <li class="w-6 h-6">
+          <img
+            class="w-full h-full"
+            src="https://api.iconify.design/logos:vueuse.svg"
+          >
+        </li>
+        <li class="w-6 h-6">
+          <img
+            class="w-full h-full"
+            src="https://api.iconify.design/skill-icons:vite-dark.svg"
+          >
+        </li>
+        <li class="w-6 h-6">
+          <img
+            class="w-full h-full"
+            src="https://api.iconify.design/skill-icons:nestjs-dark.svg"
+          >
+        </li>
+        <li class="w-6 h-6">
+          <img
+            class="w-full h-full"
+            src="https://api.iconify.design/devicon:git.svg"
+          >
+        </li>
+        <li class="w-6 h-6">
+          <img
+            class="w-full h-full"
+            src="https://api.iconify.design/skill-icons:github-dark.svg"
+          >
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
