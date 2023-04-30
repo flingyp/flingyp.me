c0433e790fccd65971a6d6e3fe4871f29d29471c
flingyp <1213563369@qq.com>
2023-04-30 23:16:54 +0800

    style: adjust project

--- components/project/Introduction.vue
+++ components/project/Introduction.vue
@@ -10,14 +10,17 @@ defineProps<ProjectIntroduction>()
 
 <template>
   <div class="flex flex-col space-y-2">
-    <div class="flex items-center space-x-2">
-      <div class="flex items-center space-x-1">
-        <Icon name="ri:git-repository-line" />
-        <span class="text-base">{{ project.name }}</span>
+    <div class="flex items-center space-x-3">
+      <div class="flex items-center space-x-2">
+        <Icon
+          name="ri:git-repository-line"
+          size="14"
+        />
+        <span class="text-base cursor-pointer">{{ project.name }}</span>
       </div>
       <span class="px-1.5 py-0.5 rounded-xl text-xs text-green-700 bg-green-100">Public</span>
     </div>
-    <span class="text-gray-600 dark:text-gray-400 text-xs">{{ project.description }}</span>
+    <span class="text-gray-600 dark:text-gray-400 text-xs md:text-sm">{{ project.description }}</span>
     <div class="text-xs dark:text-gray-200 flex items-center space-x-1">
       <p class="w-2 h-2 rounded-full bg-blue-400" />
       <span>{{ project.language }}</span>
--- pages/project.vue
+++ pages/project.vue
@@ -60,28 +60,28 @@ const projects = ref<ProjectCollection[]>([
 
 <template>
   <div class="sm:w-4/5 lg:w-3/5 xl:w-1/2 2xl:w-2/5 mx-6 sm:mx-auto mt-[2%]">
-    <h2 class="text-2xl font-semibold">
+    <h2 class="text-2xl font-semibold mb-1 md:mb-2">
       Projects
     </h2>
 
-    <p class="text-gray-600 dark:text-gray-400">
+    <p class="text-gray-600 dark:text-gray-400 mb-4">
       Personal related open source projects list
     </p>
 
-    <div class="space-y-4 mt-4">
+    <div class="space-y-4">
       <div
         v-for="(item, index) in projects"
         :key="index"
       >
-        <h3 class="text-xl mb-2">
+        <h3 class="mb-2">
           {{ item.name }}
         </h3>
 
-        <ul class="grid grid-cols-1 sm:grid-cols-2 gap-2">
+        <ul class="grid gap-3 grid-cols-1 md:grid-cols-2 md:gap-4 lg:gap-5">
           <li
             v-for="(project) in item.children"
             :key="project.name"
-            class="px-4 py-2 rounded-md cursor-pointer border border-stone-300 dark:border-stone-700"
+            class="p-4 rounded border border-neutral-300 dark:border-neutral-700"
           >
             <ProjectIntroduction :project="project" />
           </li>
