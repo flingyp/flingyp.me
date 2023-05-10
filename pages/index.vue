aaf38118f94adf2aed3a21748e27759a2e27b61d
flingyp <1213563369@qq.com>
2023-05-10 23:40:43 +0800

    feat: add github commit heatmap component

--- pages/index.vue
+++ pages/index.vue
@@ -6,5 +6,6 @@ useHead({ title: 'flingyp' })
   <div class="md:w-4/5 mx-6 md:mx-auto mt-[8%] flex flex-col items-center">
     <HomeUser class="lg:w-3/4 2xl:w-1/2" />
     <HomeIntroduction class="lg:w-3/4 mt-8 lg:mx-0 2xl:w-1/2" />
+    <HomeGithubCalendar class="mt-8 lg:w-3/4 lg:mx-0 2xl:w-1/2" />
   </div>
 </template>
